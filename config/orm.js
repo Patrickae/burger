d9b032e7868ea38d932cbdd518ac206ec5896737
Andy <Andy@Admins-MacBook-Air.local>
2017-05-29 23:03:59 -0400

    commit progress

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,33 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-	selectAll: function(){},
-	insertOne: function(){},
-	updateOne: function(){}
+	selectAll: function(table, cb){
+		var queryString = "SELECT * FROM "+table+";";
+		connection.query(queryString, function(err, result){
+			if (err) throw err;
+
+			cb(result);
+		});
+	},
+	insertOne: function(table, inputValue, cb){
+		var queryString = "INSERT INTO "+table+"(burger_name) ";
+		queryString+="VALUE ("+ inputValue + ");";
+		connection.query(queryString, function(err, result){
+			if (err) throw err;
+
+			cb(result);
+		})
+
+	},
+	updateOne: function(table, columnName, inputValue, valueId, cb){
+		var queryString = "UPDATE "+table+" SET "+columnName+" = "+inputValue;
+		queryString += "WHERE id="+valueId;
+		conection.query(queryString, function(err, result){
+			if (err) throw err;
+
+			cb(result);
+		})
+	}
 };
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,22 @@
 var orm = require("../config/orm.js");
+
+var burger = {
+	all: function(cb){
+		orm.selectAll("burgers", function(data){
+			cb(data);
+		})
+	},
+	create: function(cb, inputValue){
+		orm.insertOne("burgers", inputValue, function(data){
+			cb(data);
+		})
+
+	},
+	update: function(columnName, inputValue, valueID, cb){
+		orm.updateOne("burgers", columnName, inputValue, valueId, function(data){
+			cb(data)
+		})
+	}
+}
+
+module.exports = burger;
